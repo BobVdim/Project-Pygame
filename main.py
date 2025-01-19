0dd0262a2476d91f7e94c0428993a08b4cfcd2d1
Vadim <zmeuka.koteuka@gmail.com>
2025-01-19 16:35:40 +0300

    Инициализация игры: - добавлено окно игры с базовой настройкой Pygame и с игровым циклом

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('Начало работы с проектом')
+import pygame
+import sys
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption('Wall Jumper')
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        pygame.display.flip()
+
+    pygame.quit()
